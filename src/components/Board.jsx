ce1f27cbde9e0d870de35561aea362bb12996c29
Abhi_01 <abhishek.jadhao.er@gmail.com>
2023-05-08 22:57:39 +0530

    Introduced players 'X' and '0' to the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,18 +3,23 @@ import Square from './square';
 
 const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
   console.log(square);
 
   const handleSquareClick = clickedPosition => {
+    if (square[clickedPosition]) {
+      return;
+    }
     setSquare(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : '0';
         }
 
         return squareValue;
       });
     });
+    setIsXNext(currentIsXNext => !currentIsXNext);
   };
 
   const renderSquare = position => {
