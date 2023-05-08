1890ac55f3fa05774540b055d8dcfbc561267e7e
Abhi_01 <abhishek.jadhao.er@gmail.com>
2023-05-09 00:08:55 +0530

    lifted up to app component and shows status message to the user, xalculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import Board from './components/Board';
+import { useState } from 'react';
 import './style.scss';
+import { claculateWiner } from './winner';
 
 function App() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = claculateWiner(square);
+  const nextPlayr = isXNext ? 'X' : '0';
+
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${nextPlayr}`;
+
+  console.log(winner);
+
+  const handleSquareClick = clickedPosition => {
+    if (square[clickedPosition] || winner) {
+      return;
+    }
+    setSquare(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : '0';
+        }
+
+        return squareValue;
+      });
+    });
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
   return (
     <div className="app">
-      <Board />
+      {statusMessage}
+      <Board square={square} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,7 @@
-import { useState } from 'react';
 import Square from './square';
 
-const Board = () => {
-  const [square, setSquare] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  console.log(square);
-
-  const handleSquareClick = clickedPosition => {
-    if (square[clickedPosition]) {
-      return;
-    }
-    setSquare(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : '0';
-        }
-
-        return squareValue;
-      });
-    });
-    setIsXNext(currentIsXNext => !currentIsXNext);
-  };
+const Board = ({ square, handleSquareClick }) => {
+  //console.log(square);
 
   const renderSquare = position => {
     return (
